5362cc359eba001c91369674b379ac165ed568de
Muhammad Muqsith Giga Saputra <123441826+gigasaputra7701@users.noreply.github.com>
2024-11-10 17:15:33 +0700

    final

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,19 @@
-import ee from "../eventEmitter";
+import { createStore, compose } from "redux";
+import { rootReducer } from "../actions";
 
-let store = {
-  displayedExpression: 0,
-  get curExpression() {
-    return this.displayedExpression;
-  },
-  set newExpression(curExpression) {
-    this.displayedExpression = curExpression;
-    ee.emitEvent("displayUpdate", [this.curExpression]);
-    ee.emitEvent("historyUpdate", [this.curExpression]);
-  },
-};
+const initialState = {};
+const enhancers = [];
+
+if (process.env.NODE_ENV === "development") {
+  const devToolExtensions = window.devToolExtensions;
+
+  if (typeof devToolExtensions === "function") {
+    enhancers.push(devToolExtensions());
+  }
+}
+
+const composedEnhancers = compose(...enhancers);
+
+const store = createStore(rootReducer, initialState, composedEnhancers);
 
 export default store;
